0aa473fd5c4c7d17d5a8d161648743625c0bed1c
kumush2010 <joramirzayevmuhammad777@gmail.com>
2022-08-31 22:22:25 +0500

    Cloack

---
+++ app.js
@@ -0,0 +1,30 @@
+function showtime(){
+let soat, time, hour, minute, second;
+soat = document.getElementById('soat');
+time = new Date();
+hour = time.getHours();
+minute = time.getMinutes();
+second = time.getSeconds();
+
+if (hour >=12) {
+    hour = hour - 12 + ' PM';
+}else {
+    hour = hour + ' AM';
+}
+
+if (hour <10) {
+    hour = '0' + hour;
+}
+
+if (minute <10) {
+    minute = '0' + minute;
+}
+if (second <10) {
+    second = '0' + second;
+}
+
+soat.innerHTML = `${hour} : ${minute} : ${second}`;
+setTimeout(showtime, 1000);
+}
+
+showtime();
